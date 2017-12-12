2e4bfa4cddc1024af4d9a6efa5754ef775cb35ea
miguelch96 <miguelangelca96@outlook.com>
2017-12-12 10:49:13 -0500

    Basic Authentication Added

--- YelpCampProject/app.js
+++ YelpCampProject/app.js
@@ -2,14 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
-var User = require("./models/user");
 var Comment = require("./models/comment");
 var seedDB = require("./seed");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var User = require("./models/user");
+
 
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/yelpcampDB",{
+mongoose.connect("mongodb://localhost/yelpcampDB_2",{
     useMongoClient: true
 });
 
@@ -18,13 +21,23 @@ var app = express();
 
 // view engine setup
 app.set('view engine', 'ejs');
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + 'public'));
-
-
 seedDB();
+
+// PASSPORT CONFIGURATION
+
+app.use(require("express-session")({
+    secret: "The secret is learn many thing",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize())
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/",function (req, res) {
@@ -137,6 +150,35 @@ app.post("/campgrounds/:id/comments",function (req,res) {
         }
     })
 });
+
+// AUTH ROUTES
+
+// show register form
+app.get("/register",function (req, res) {
+    res.render("register")
+})
+
+app.post("/register",function (req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser,req.body.password,function (err, user) {
+        if(err){
+            console.log(err);
+            return res.render("register")
+        }
+        else{
+            passport.authenticate("local")(req,res,function () {
+                res.redirect("/campgrounds")
+            })
+        }
+    })
+})
+
+
+app.get("/login",function (req, res) {
+    res.render("login")
+})
+
+
 
 //FOR INVALID ROUTES
 app.get("*",function (req,res) {
--- YelpCampProject/models/user.js
+++ YelpCampProject/models/user.js
@@ -1,7 +1,14 @@
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose")
+
 
 var userSchema = new mongoose.Schema({
-    name: String
+    username: String,
+    password: String
 },{usePushEach: true});
+
+userSchema.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model("User",userSchema)
